ee27b3e3aecdbecd5bd2ae96d13ef0742b833738
dshibidzhiev <dshibidzhiev@ubic.tech>
2024-01-22 00:13:13 +0300

    middleware.ts: add Clerk Auth Provider and Middleware

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,17 @@
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+
+const PlatformLayout = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  
+  return (
+    <ClerkProvider>
+      {children}
+    </ClerkProvider>
+  )
+};
+  
+  export default PlatformLayout;
+  
